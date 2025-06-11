3938966e265cd57d19ae75f946259001dda2a899
miguelr215 <drexelramos@gmail.com>
2025-06-10 20:51:18 -0400

    api integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,110 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from 'react';
+import { GoogleGenAI } from '@google/genai';
 import './App.css';
 
-function App() {
-	const [count, setCount] = useState(0);
+interface Message {
+	role: 'user' | 'assistant';
+	content: string;
+	timestamp: string;
+}
 
-	useEffect(() => {
-		console.log(import.meta.env.VITE_GOOGLE_GEN_API_KEY);
-	}, []);
+function App() {
+	// const googleGenApiKey = import.meta.env.VITE_GOOGLE_GEN_API_KEY;
+	const [messages, setMessages] = useState<Message[]>([]);
+	const [newMessage, setNewMessage] = useState<string>('');
+
+	const ai = new GoogleGenAI({
+		apiKey: 'AIzaSyDZP4OccsJ9rijlW7S4PlnVo87Y_5ozmOQ',
+	});
+
+	async function getAIMessage(message: string) {
+		try {
+			const res = await ai.models.generateContent({
+				model: 'gemini-2.0-flash',
+				contents: message,
+			});
+
+			console.log('AI response:', res);
+			return res.text;
+		} catch (error) {
+			console.log('error getting AI message:', error);
+		}
+	}
+
+	async function sendMessage(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const message = newMessage;
+
+		setMessages((prev) => [
+			...prev,
+			{
+				content: message,
+				role: 'user',
+				timestamp: new Date().toLocaleTimeString(),
+			},
+		]);
+
+		const aiResponse = await getAIMessage(message);
+		console.log('ai res:', aiResponse);
+
+		setMessages((prev) => [
+			...prev,
+			{
+				content: aiResponse || 'No response from AI',
+				role: 'assistant',
+				timestamp: new Date().toLocaleTimeString(),
+			},
+		]);
+
+		setNewMessage('');
+	}
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
+			<main>
+				<section className="header">
+					<h1>Gemini Chat</h1>
+					<div>
+						<a
+							href="https://ai.google.dev/gemini-api/docs"
+							target="_blank"
+						>
+							Gemini API Docs
+						</a>
+					</div>
+				</section>
+				<section>
+					{messages.map((message) => {
+						return (
+							<div
+								key={message.timestamp}
+								className={`message ${message.role}`}
+							>
+								<time>{message.timestamp}</time>
+								<div>
+									{message.content
+										.split('\n')
+										.map((line, idx) => (
+											<p key={idx}>{line}</p>
+										))}
+								</div>
+							</div>
+						);
+					})}
+				</section>
+			</main>
+
+			<footer>
+				<form onSubmit={sendMessage}>
+					<input
+						value={newMessage}
+						onChange={(e) => setNewMessage(e.target.value)}
+						type="text"
+						placeholder="Enter your message..."
 					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+					<button type="submit">Send</button>
+				</form>
+			</footer>
 		</>
 	);
 }
